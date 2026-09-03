27e01bd3235cd531c95e2a4b0647e1aa419ae845
Hassara17 <cse.20211026@gmail.com>
2026-09-03 14:01:38 +0530

    fix CORS for Vercel deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -29,7 +29,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://event-registration-system-hassan-20f1.vercel.app",
+        "https://event-registration-system-g303ykami-hassan-20f1.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
